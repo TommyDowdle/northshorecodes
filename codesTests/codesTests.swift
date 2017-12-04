8461776b5d996bd40be2ee5635161fbb5bc25c28
Shannon Dowdle <shannondowdle@Shannons-MacBook-Air.local>
2017-12-04 16:44:27 -0600

    Initial Commit

---
+++ codesTests/codesTests.swift
@@ -0,0 +1,36 @@
+//
+//  codesTests.swift
+//  codesTests
+//
+//  Created by Shannon Dowdle on 12/4/17.
+//  Copyright © 2017 Tommy Dowdle. All rights reserved.
+//
+
+import XCTest
+@testable import codes
+
+class codesTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
